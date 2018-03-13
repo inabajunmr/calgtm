533365c8813b5f8167fc38b687237c7a6dfd488f
inabajunmr <inabajun.for.regi@gmail.com>
2018-03-14 01:36:00 +0900

    border

--- test.py
+++ test.py
@@ -2,7 +2,27 @@ from PIL import Image, ImageFont, ImageDraw
 
 img = Image.open('./test.jpg')
 draw = ImageDraw.Draw(img)
-font = ImageFont.truetype("LiberationSans-Bold.ttf", 42)
-draw.text((0, 0),"LGTM!",(255,255,255),font=font)
+font = ImageFont.truetype("LiberationSans-Bold.ttf", 100)
+text = "LGTM!"
+shadowcolor = "yellow"
+fillcolor = "red"
+
+draw.text((0, 0), text,(255,255,255),font=font)
+
+# https://mail.python.org/pipermail/image-sig/2009-May/005681.html
+x, y = 10, 10
+draw.text((x-1, y-1), text, font=font, fill=shadowcolor)
+draw.text((x+1, y-1), text, font=font, fill=shadowcolor)
+draw.text((x-1, y+1), text, font=font, fill=shadowcolor)
+draw.text((x+1, y+1), text, font=font, fill=shadowcolor)
+
+draw.text((x-1, y), text, font=font, fill=shadowcolor)
+draw.text((x+1, y), text, font=font, fill=shadowcolor)
+draw.text((x, y-1), text, font=font, fill=shadowcolor)
+draw.text((x, y+1), text, font=font, fill=shadowcolor)
+
+draw.text((x, y), text, font=font, fill=fillcolor)
+
+
 img.save('out.jpg')
 
