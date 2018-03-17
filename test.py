8937cdc6b0f790aff9b18730623b5c6e2a8ddbd6
inabajunmr <inabajun.for.regi@gmail.com>
2018-03-17 15:27:31 +0900

    compress too big image and all format convert jpg

--- test.py
+++ test.py
@@ -2,17 +2,25 @@ from PIL import Image, ImageFont, ImageDraw
 import urllib.request
 import io
 
-def lgtm(img, fillcolor="white", shadowcolor="black"):
+def lgtm(img_binary, fillcolor="white", shadowcolor="black"):
     ## TODO get size of image
     ## TODO adjust font size
     ## TODO adjust place of text
-    font_size = 100;
+
+    img = Image.open(img_binary)
+    # convert to jpg
+    img.convert("RGB")
+    # compress for too big image
+    img.thumbnail((1024, 1024), Image.ANTIALIAS)
+
     draw = ImageDraw.Draw(img)
+
+    font_size = 20
     font = ImageFont.truetype("LiberationSans-Bold.ttf", font_size)
     text = "LGTM!"
 
-    # place of text
-    x, y = 10, 10
+    # adjust place of text(https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil)
+    x, y = 90, 50
 
     # draw border
     draw.text((x, y-1), text, font=font, fill=shadowcolor)
@@ -33,9 +41,8 @@ def get_img(url):
     req = urllib.request.Request(url)
     image_read = urllib.request.urlopen(req).read()
     img_binary = io.BytesIO(image_read)
-    img = Image.open(img_binary)
-    return img
+    return img_binary
 
-img = get_img('https://pbs.twimg.com/media/DWZ0jPVV4AAFm7S.jpg:large')
+img_binary = get_img('https://pbs.twimg.com/media/DWZ0jPVV4AAFm7S.jpg:large')
 ## TODO compress image
-lgtm(img)
+lgtm(img_binary)
